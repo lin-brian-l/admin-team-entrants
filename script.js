42229e2672c985a8d0227dadc015a956839a50b4
lin-brian-l <lin.brian.l@gmail.com>
2018-09-08 04:46:32 -0400

    Make xhr requests to resolve CORS and refactor callbacks into async and awaits.

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 const addJQuery = () => {
-	// console.log("adding jQuery");
 	var jq = document.createElement('script');
 	jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
-	document.getElementsByTagName('head')[0].appendChild(jq);	
-	// console.log("before setTimeout");
-	// setTimeout(() => { 
-	// 	jQuery.noConflict(); 
-	// 	console.log("after noConflict()");
-	// }, 1000)
+	document.getElementsByTagName('head')[0].appendChild(jq);
 }
 
-const startScript = () => {
-	// console.log("starting script");
+const startScript = async () => {
 	addJQuery();
-	const tagDivs = $(".list-unstyled.striped.phase-seed-list").find(".gamertag-title");
-	tagDivs.each((index, child) => {
-		console.log(child.innerText);
+	const tourneyData = getTourneyData();
+	const tourneyAPI = `https://api.smash.gg/tournament/${tourneyData.name}?expand[]=phase&expand[]=groups&expand[]=event&expand[]=participants`;
+	const tourneyAPIResults = JSON.parse(await makeCorsRequest(tourneyAPI));
+	console.log("api result: " + tourneyAPIResults.result);
+
+	// const tagDivs = $(".list-unstyled.striped.phase-seed-list").find(".gamertag-title");
+	// tagDivs.each((index, child) => {
+	// 	console.log(child.innerText);
+	// })
+}
+
+const getTourneyData = () => {
+	const pageUrl = window.location.href;
+	const name = pageUrl.match(/(?<=tournament\/)[^\/]*/)[0];
+	const eventAndPhaseIdArray = pageUrl.match(/(?<=seeding\/)[^\$]*/)[0].split("/");
+	const eventId = eventAndPhaseIdArray[0];
+	const phaseId = eventAndPhaseIdArray[1];
+	return { name, eventId, phaseId };
+}
+
+const makeCorsRequest = (url) => {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = (e) => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(xhr.response);
+        } else {
+          reject(xhr.status);
+        }
+      }
+    }
+    xhr.ontimeout = () => {
+      reject('timeout');
+    }
+    xhr.open('get', url, true);
+    xhr.send();
 	})
 }
 
-// find tourney name and phase ID from URL
-
 // https://api.smash.gg/tournament/api-testing?expand[]=phase&expand[]=groups&expand[]=event&expand[]=participants
+// https://api.smash.gg/tournament/api-testing?expand[]=groups // use this
 // grab group IDs for specific phase
 
 // https://api.smash.gg/phase_group/733190?expand[]=sets&expand[]=standings&expand[]=entrants&expand[]=seeds&expand[]=participants
